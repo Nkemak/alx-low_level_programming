d43fdfddac0ba901aa391b477cf846071f320a0e
Nkemak <akolamprecious@gmail.com>
2023-07-13 15:54:52 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include  <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programing is like a multilingualpuzzle");
+	return (0);
+}
